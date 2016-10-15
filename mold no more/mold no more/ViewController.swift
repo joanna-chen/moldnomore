acf19c765391451475c0a4e7647103103ff16594
joanna-chen <joannachen.x@gmail.com>
2016-10-14 23:06:52 -0400

    initial ios app commit

---
+++ mold no more/mold no more/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mold no more
+//
+//  Created by Joanna Chen on 2016-10-14.
+//  Copyright © 2016 Joanna Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
