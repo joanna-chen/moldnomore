617587dd7d9a02b20109b9fca33d5041d7d71228
joanna-chen <joannachen.x@gmail.com>
2016-10-14 23:53:33 -0400

    added tesseract

---
+++ mold no more/-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  -Bridging-Header.h
+//  mold no more
+//
+//  Created by Joanna Chen on 2016-10-14.
+//  Copyright © 2016 Joanna Chen. All rights reserved.
+//
+
+#ifndef _Bridging_Header_h
+#define _Bridging_Header_h
+
+#import <TesseractOCR/TesseractOCR.h>
+
+
+#endif /* _Bridging_Header_h */
--- mold no more/mold no more/ViewController.swift
+++ mold no more/mold no more/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import TesseractOCR
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, G8TesseractDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var tesseract:G8Tesseract = G8Tesseract(language:"eng+ita");
+        
+        tesseract.delegate = self;
+        tesseract.charWhitelist = "01234567890";
+        tesseract.image = UIImage(named: "image_sample.jpg");
+        tesseract.recognize();
+        
+        NSLog("%@", tesseract.recognizedText);
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func shouldCancelImageRecognitionForTesseract(tesseract: G8Tesseract!) -> Bool {
+        return false; // return true if you need to interrupt tesseract before it finishes
+    }
 
 }
 
